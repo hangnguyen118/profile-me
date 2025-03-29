945572aa62ccea21976e07ac9840dab02f531e4c
hangnguyen118 <hangnguyen118.wy@gmail.com>
2025-03-29 18:53:06 +0700

    create about and reset password page

---
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [MatIcon],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.css'
+})
+export class ResetPasswordComponent {
+
+}
